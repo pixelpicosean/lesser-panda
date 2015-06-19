4f5f06a53e087eb98ed657c4e0643b649028a956
Sean Bohan <pixelpicosean@gmail.com>
2015-06-19 11:07:22 +0800

    Update timer module

--- src/engine/timer.js
+++ src/engine/timer.js
@@ -1,6 +1,5 @@
 /**
     @module timer
-    @namespace game
 **/
 game.module(
     'engine.timer'
@@ -9,23 +8,37 @@ game.module(
 'use strict';
 
 /**
-    Basic timer.
     @class Timer
-    @extends game.Class
     @constructor
     @param {Number} [ms]
 **/
 game.createClass('Timer', {
+    /**
+        Timer's target time.
+        @property {Number} target
+    **/
     target: 0,
+    /**
+        Timer's base time.
+        @property {Number} base
+    **/
     base: 0,
-    last: 0,
-    pauseTime: 0,
-    
+    /**
+        @property {Number} _last
+        @private
+    **/
+    _last: 0,
+    /**
+        @property {Number} _pauseTime
+        @private
+    **/
+    _pauseTime: 0,
+
     init: function(ms) {
-        this.last = game.Timer.time;
+        this._last = game.Timer.time;
         this.set(ms);
     },
-    
+
     /**
         Set time for timer.
         @method set
@@ -36,32 +49,34 @@ game.createClass('Timer', {
         this.target = ms;
         this.reset();
     },
-    
+
     /**
         Reset timer.
         @method reset
     **/
     reset: function() {
         this.base = game.Timer.time;
-        this.pauseTime = 0;
+        this._pauseTime = 0;
     },
-    
+
     /**
         Get time since last delta.
         @method delta
+        @return {Number} delta
     **/
     delta: function() {
-        var delta = game.Timer.time - this.last;
-        this.last = game.Timer.time;
-        return this.pauseTime ? 0 : delta;
+        var delta = game.Timer.time - this._last;
+        this._last = game.Timer.time;
+        return this._pauseTime ? 0 : delta;
     },
-    
+
     /**
         Get time since start.
         @method time
+        @return {Number} time
     **/
     time: function() {
-        var time = (this.pauseTime || game.Timer.time) - this.base - this.target;
+        var time = (this._pauseTime || game.Timer.time) - this.base - this.target;
         return time;
     },
 
@@ -70,7 +85,7 @@ game.createClass('Timer', {
         @method pause
     **/
     pause: function() {
-        if (!this.pauseTime) this.pauseTime = game.Timer.time;
+        if (!this._pauseTime) this._pauseTime = game.Timer.time;
     },
 
     /**
@@ -78,24 +93,57 @@ game.createClass('Timer', {
         @method resume
     **/
     resume: function() {
-        if (this.pauseTime) {
-            this.base += game.Timer.time - this.pauseTime;
-            this.pauseTime = 0;
+        if (this._pauseTime) {
+            this.base += game.Timer.time - this._pauseTime;
+            this._pauseTime = 0;
         }
     }
 });
 
 game.addAttributes('Timer', {
-    last: 0,
-    time: Number.MIN_VALUE,
-    speedFactor: 1,
+    /**
+        Current time.
+        @attribute {Number} time
+    **/
+    time: 0,
+    /**
+        Main timer's speed factor.
+        @attribute {Number} speed
+        @default 1
+    **/
+    speed: 1,
+    /**
+        Main timer's minimum fps.
+        @attribute {Number} minFPS
+        @default 20
+    **/
     minFPS: 20,
-
+    /**
+        Main timer's delta (ms).
+        @attribute {Number} delta
+    **/
+    delta: 0,
+    /**
+        @attribute {Number} _last
+        @private
+    **/
+    _last: 0,
+    /**
+        @attribute {Number} _realDelta
+        @private
+    **/
+    _realDelta: 0,
+    /**
+        Update main timer.
+        @attribute {Function} update
+    **/
     update: function() {
         var now = Date.now();
-        if (!game.Timer.last) game.Timer.last = now;
-        game.Timer.time += Math.min((now - game.Timer.last), 1000 / game.Timer.minFPS) * game.Timer.speedFactor;
-        game.Timer.last = now;
+        if (!this._last) this._last = now;
+        this._realDelta = now - this._last;
+        this.delta = Math.min(this._realDelta, 1000 / this.minFPS) * this.speed;
+        this.time += this.delta;
+        this._last = now;
     }
 });
 
