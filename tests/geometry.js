c296da8bb444eb65d405d1d41462511cee807b24
Sean Bohan <pixelpicosean@gmail.com>
2015-07-19 07:44:56 +0800

    Add some tests of geometry module.

--- tests/geometry.js
+++ tests/geometry.js
@@ -7,9 +7,48 @@ describe('Vector', function() {
     b = new game.Vector();
   });
 
-  it('should has x and y properties', function() {
-    expect(a.x).toBeDefined();
-    expect(a.y).toBeDefined();
+  describe('create without any params', function() {
+    it('its x and y value should be 0', function() {
+      expect(a.x).toBe(0);
+      expect(a.y).toBe(0);
+    })
+  });
+
+  describe('.set', function() {
+    it('with a single value, both of its x and y should be set to it', function () {
+      a.set(10);
+
+      expect(a.x).toBe(10);
+      expect(a.y).toBe(10);
+    });
+
+    it('with two values, x should equal the first one and y should equal the second one', function () {
+      a.set(1, 2);
+
+      expect(a.x).toBe(1);
+      expect(a.y).toBe(2);
+    });
+  });
+
+  describe('.clone', function() {
+    it('should return a different Vector object', function() {
+      var c = a.clone();
+      expect(c).not.toBe(a);
+    });
+    it('and the returned object should has the same value', function() {
+      var c = a.clone();
+      expect(c.x).toBe(a.x);
+      expect(c.y).toBe(a.y);
+    });
+  });
+
+  describe('.copy', function() {
+    it('should set self\'s value to be the same of target', function() {
+      a.set(8, 9);
+      b.copy(a);
+      expect(b.x).toBe(a.x);
+      expect(b.y).toBe(a.y);
+    });
   });
 
 });
