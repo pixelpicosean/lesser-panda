9dfa6593ef6024350a6639294c5c1a9675e4cb1f
Sean Bohan <pixelpicosean@gmail.com>
2015-07-21 07:57:08 +0800

    Add tests for some method of game.Vector.

--- tests/geometry.js
+++ tests/geometry.js
@@ -51,4 +51,80 @@ describe('Vector', function() {
     });
   });
 
+  describe('.add', function() {
+    it('should add param to both x and y if it\'s a number', function() {
+      a.set(1, 1);
+      a.add(2);
+      expect(a.x).toBe(1 + 2);
+      expect(a.y).toBe(1 + 2);
+    });
+
+    it('should add first param to x and second to y if both of them are numbers', function() {
+      a.set(1, 1);
+      a.add(2, 3);
+      expect(a.x).toBe(1 + 2);
+      expect(a.y).toBe(1 + 3);
+    });
+
+    it('should add param.x to self.x and param.y to self.y if it\'s a vector', function() {
+      a.set(1, 1);
+      b.set(2, 3);
+      a.add(b);
+      expect(a.x).toBe(1 + b.x);
+      expect(a.y).toBe(1 + b.y);
+    });
+  });
+
+  describe('.subtract', function() {
+    it('should subtract param from both x and y if it\'s a number', function() {
+      a.set(1, 1);
+      a.subtract(2);
+      expect(a.x).toBe(1 - 2);
+      expect(a.y).toBe(1 - 2);
+    });
+
+    it('should subtract first param from x and second from y if both of them are numbers', function() {
+      a.set(1, 1);
+      a.subtract(2, 3);
+      expect(a.x).toBe(1 - 2);
+      expect(a.y).toBe(1 - 3);
+    });
+
+    it('should subtract x from self.x and y from self.y if it\'s a vector', function() {
+      a.set(1, 1);
+      b.set(2, 3);
+      a.subtract(b);
+      expect(a.x).toBe(1 - b.x);
+      expect(a.y).toBe(1 - b.y);
+    });
+  });
+
+  describe('.multiply', function() {
+    it('should multiply both x and y with param if it\'s a number', function() {
+      a.set(1, 1);
+      a.multiply(2);
+      expect(a.x).toBe(1 * 2);
+      expect(a.y).toBe(1 * 2);
+    });
+
+    it('should multiply x with the first param and y with the second if both of them are numbers', function() {
+      a.set(1, 1);
+      a.multiply(2, 3);
+      expect(a.x).toBe(1 * 2);
+      expect(a.y).toBe(1 * 3);
+    });
+
+    it('should multiply self.x with value.x and self.y with value.y if param is a vector', function() {
+      a.set(1, 1);
+      b.set(2, 3);
+      a.multiply(b);
+      expect(a.x).toBe(1 * b.x);
+      expect(a.y).toBe(1 * b.y);
+    });
+
+    it('should be called "scale" instead', function() {
+      expect('multiply').toBe('scale');
+    });
+  });
+
 });
