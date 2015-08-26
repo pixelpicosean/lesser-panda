dd0ff7093f3abf335f6f4001d83bef22d4cb2e4d
Sean Bohan <pixelpicosean@gmail.com>
2015-08-26 23:29:26 +0800

    Use Object.assign to extend Scene.

--- src/game/main.js
+++ src/game/main.js
@@ -7,19 +7,17 @@ game.module(
   game.addAsset('logo.png');
 
   function Main() {
-    // Override Scene properties before super call
-    this.backgroundColor = 0xb9bec7;
-
-    // Call Scene ctor
     game.Scene.call(this);
 
-    // Do what you want below
     var logo = new game.Sprite('logo.png')
       .center()
       .addTo(this.stage);
   };
   Main.prototype = Object.create(game.Scene.prototype);
-  Main.prototype.constructor = Main;
+  Object.assign(Main.prototype, {
+    backgroundColor: 0xb9bec7,
+    constructor: Main,
+  });
 
   game.Main = Main;
 
