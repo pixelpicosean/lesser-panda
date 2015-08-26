1a0634726d97e961e8ddbcc66675f7dda1ed72bc
Sean Bohan <pixelpicosean@gmail.com>
2015-08-26 12:28:49 +0800

    Use prototype based OOP for analytics module.

--- src/engine/analytics.js
+++ src/engine/analytics.js
@@ -15,75 +15,68 @@ game.module(
     @constructor
     @param {String} id
   **/
-  game.createClass('Analytics', {
-    /**
-      Current tracking id.
-      @property {String} trackId
-    **/
-    trackId: null,
-    clientId: null,
+  function Analytics(id) {
+    this.trackId = id || game.Analytics.id;
 
-    init: function(id) {
-      this.trackId = id || game.Analytics.id;
+    if (!navigator.onLine) return;
 
-      if (!navigator.onLine) return;
+    if (game.device.cocoonJS) {
+      this.clientId = Date.now();
+      var request = new XMLHttpRequest();
+      var params = 'v=1&tid=' + this.trackId + '&cid=' + this.clientId + '&t=pageview&dp=%2F';
+      request.open('POST', 'http://www.google-analytics.com/collect', true);
+      request.send(params);
+    } else {
+      (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+          (i[r].q = i[r].q || []).push(arguments)
+        };
 
-      if (game.device.cocoonJS) {
-        this.clientId = Date.now();
-        var request = new XMLHttpRequest();
-        var params = 'v=1&tid=' + this.trackId + '&cid=' + this.clientId + '&t=pageview&dp=%2F';
-        request.open('POST', 'http://www.google-analytics.com/collect', true);
-        request.send(params);
-      } else {
-        (function(i, s, o, g, r, a, m) {
-          i['GoogleAnalyticsObject'] = r;
-          i[r] = i[r] || function() {
-            (i[r].q = i[r].q || []).push(arguments)
-          };
+        i[r].l = 1 * new Date();
+        a = s.createElement(o);
+        m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m);
+      })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-          i[r].l = 1 * new Date();
-          a = s.createElement(o);
-          m = s.getElementsByTagName(o)[0];
-          a.async = 1;
-          a.src = g;
-          m.parentNode.insertBefore(a, m);
-        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+      ga('create', id, 'auto');
+      ga('send', 'pageview');
+    }
+  }
 
-        ga('create', id, 'auto');
-        ga('send', 'pageview');
-      }
-    },
+  /**
+    Send event to analytics.
+    @method send
+    @param {String} category
+    @param {String} action
+    @param {String} [label]
+    @param {String} [value]
+  **/
+  Analytics.prototype.send = function send(category, action, label, value) {
+    if (!navigator.onLine) return;
 
-    /**
-      Send event to analytics.
-      @method send
-      @param {String} category
-      @param {String} action
-      @param {String} [label]
-      @param {String} [value]
-    **/
-    send: function(category, action, label, value) {
-      if (!navigator.onLine) return;
+    if (game.device.cocoonJS) {
+      var request = new XMLHttpRequest();
+      var params = 'v=1&tid=' + this.trackId + '&cid=' + this.clientId + '&t=event&ec=' + category + '&ea=' + action;
+      if (typeof label !== 'undefined') params += '&el=' + label;
+      if (typeof value !== 'undefined') params += '&ev=' + value;
+      request.open('POST', 'http://www.google-analytics.com/collect', true);
+      request.send(params);
+    } else {
+      ga('send', 'event', category, action, label, value);
+    }
+  };
 
-      if (game.device.cocoonJS) {
-        var request = new XMLHttpRequest();
-        var params = 'v=1&tid=' + this.trackId + '&cid=' + this.clientId + '&t=event&ec=' + category + '&ea=' + action;
-        if (typeof label !== 'undefined') params += '&el=' + label;
-        if (typeof value !== 'undefined') params += '&ev=' + value;
-        request.open('POST', 'http://www.google-analytics.com/collect', true);
-        request.send(params);
-      } else {
-        ga('send', 'event', category, action, label, value);
-      }
-    },
-  });
-
-  game.addAttributes('Analytics', {
+  game.addAttributes(Analytics, {
     /**
       Tracking id for analytics.
       @attribute {String} id
     **/
     id: '',
   });
+
+  game.Analytics = Analytics;
 
 });
