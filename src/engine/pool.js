2336b0a4c0403b539e2da4be0066b3f9f4b09cd0
Sean Bohan <pixelpicosean@gmail.com>
2015-08-26 12:30:49 +0800

    Use prototype based OOP for pool module.

--- src/engine/pool.js
+++ src/engine/pool.js
@@ -13,45 +13,47 @@ game.module(
     @class Pool
     @extends game.Class
   **/
-  game.createClass('Pool', {
-    /**
-      Create new pool.
-      @method create
-      @param {String} pool Name of the pool.
-      @return {Boolean} Returns false, if pool already exists.
-    **/
-    create: function(pool) {
-      if (!this[pool]) {
-        this[pool] = [];
-        return true;
-      }
+  function Pool() {}
 
-      return false;
-    },
-
-    /**
-      Get object from pool.
-      @method get
-      @param {String} pool Name of the pool.
-      @return {Object} Returns false, if pool not found or empty.
-    **/
-    get: function(pool) {
-      if (!this[pool] || this[pool].length === 0) return false;
-      else return this[pool].pop();
-    },
-
-    /**
-      Put object to pool.
-      @method put
-      @param {String} pool Name of the pool.
-      @param {Object} object Object to put to the pool.
-      @return {Boolean} Returns false, if pool not found.
-    **/
-    put: function(pool, object) {
-      if (!this[pool]) return false;
-      this[pool].push(object);
+  /**
+    Create new pool.
+    @method create
+    @param {String} pool Name of the pool.
+    @return {Boolean} Returns false, if pool already exists.
+  **/
+  Pool.prototype.create = function create(pool) {
+    if (!this[pool]) {
+      this[pool] = [];
       return true;
-    },
-  });
+    }
+
+    return false;
+  };
+
+  /**
+    Get object from pool.
+    @method get
+    @param {String} pool Name of the pool.
+    @return {Object} Returns false, if pool not found or empty.
+  **/
+  Pool.prototype.get = function get(pool) {
+    if (!this[pool] || this[pool].length === 0) return false;
+    else return this[pool].pop();
+  };
+
+  /**
+    Put object to pool.
+    @method put
+    @param {String} pool Name of the pool.
+    @param {Object} object Object to put to the pool.
+    @return {Boolean} Returns false, if pool not found.
+  **/
+  Pool.prototype.put = function put(pool, object) {
+    if (!this[pool]) return false;
+    this[pool].push(object);
+    return true;
+  };
+
+  game.Pool = Pool;
 
 });
