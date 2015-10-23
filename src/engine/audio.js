49390c56cb7d79f4dc6873a7faa6c2f310a759dc
Sean Bohan <pixelpicosean@gmail.com>
2015-10-23 16:48:30 +0800

    Create a simple audio module to wrap Howler and register a sound file loader to loader manager.

---
+++ src/engine/audio.js
@@ -0,0 +1,55 @@
+import { Howl } from './howler.core';
+import loaderMgr from './loader-manager';
+
+let sounds = {};
+let soundsToLoadCount = 0;
+let soundsLoadedCount = 0;
+
+// Callbacks
+let progressCB;
+let completeCB;
+
+function addSound(src, id) {
+  if (sounds[id || src] || sounds[src]) {
+    return;
+  }
+
+  let snd = new Howl({
+    src: 'media/' + src,
+    preload: false,
+    onload: onload.bind(undefined, snd),
+  });
+  sounds[id || src] = snd;
+
+  soundsToLoadCount += 1;
+}
+
+function onload(snd) {
+  soundsLoadedCount += 1;
+  progressCB && progressCB(snd);
+
+  if (soundsLoadedCount === soundsToLoadCount && completeCB) {
+    setTimeout(completeCB, 0);
+
+    // Remove ref to callbacks
+    completeCB = null;
+    progressCB = null;
+  }
+}
+
+loaderMgr.registerLoader({
+  start: function(onComplete, onProgress) {
+    progressCB = onProgress;
+    completeCB = onComplete;
+
+    for (let s in sounds) sounds[s].load();
+  },
+  getAssetsLength: function() {
+    return soundsToLoadCount;
+  },
+});
+
+export default {
+  sounds,
+  addSound,
+};
