a55e1ddad56cb0f9b4c2d371e41e709ef05bea5c
Sean Bohan <pixelpicosean@gmail.com>
2015-10-24 15:14:35 +0800

    Test keyboard module, and use function for scene class instead of "class".

--- src/game/main.js
+++ src/game/main.js
@@ -1,21 +1,24 @@
 import loader from 'engine/loader';
 import audio from 'engine/audio';
+import keyboard from 'engine/keyboard';
 
 loader.addAsset('KenPixel.fnt');
 loader.addAsset('KenPixel.png', 'fontTexture');
 
 audio.addSound('tune2.mp3');
 
-class LoadingScene {
-  constructor() {
-    loader.on('progress', (progress) => {
-      console.log(`Load ${Math.floor(progress * 100)}%`);
-    });
-    loader.once('complete', () => {
-      console.log('Assets loaded!');
-    });
-    loader.start();
-  }
+function LoadingScene() {
+  loader.on('progress', (progress) => {
+    console.log(`Load ${Math.floor(progress * 100)}%`);
+  });
+  loader.once('complete', () => {
+    console.log('Assets loaded!');
+  });
+  loader.start();
+
+  keyboard.once('keydown', function(key) {
+    console.log(`${key} is pressed.`);
+  });
 }
 
 new LoadingScene();
