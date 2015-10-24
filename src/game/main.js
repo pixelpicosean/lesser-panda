c7298e759122ae36d62817324377b77328f9cb4c
Sean Bohan <pixelpicosean@gmail.com>
2015-10-24 20:22:35 +0800

    Test scene and import timer module.

--- src/game/main.js
+++ src/game/main.js
@@ -3,12 +3,17 @@ import audio from 'engine/audio';
 import keyboard from 'engine/keyboard';
 import storage from 'engine/storage';
 
+import Scene from 'engine/scene';
+import Timer from 'engine/timer';
+
 loader.addAsset('KenPixel.fnt');
 loader.addAsset('KenPixel.png', 'fontTexture');
 
 audio.addSound('tune2.mp3');
 
 function LoadingScene() {
+  Scene.call(this);
+
   loader.on('progress', (progress) => {
     console.log(`Load ${Math.floor(progress * 100)}%`);
   });
@@ -23,6 +28,10 @@ function LoadingScene() {
 
   storage.set('name', 'Sean');
   console.log(`get data from storage, name = ${storage.get('name')}`);
+
+  console.log(this.addTimer);
 }
+LoadingScene.prototype = Object.create(Scene.prototype);
+LoadingScene.prototype.constructor = LoadingScene;
 
 new LoadingScene();
