e376d23de0e9831193a1006031db53aca3cc9e7f
Sean Bohan <pixelpicosean@gmail.com>
2015-10-25 19:09:44 +0800

    Test whether device detecting works.

--- src/game/main.js
+++ src/game/main.js
@@ -6,6 +6,7 @@ import audio from 'engine/audio';
 import keyboard from 'engine/keyboard';
 import storage from 'engine/storage';
 import Timer from 'engine/timer';
+import device from 'engine/device';
 
 import PIXI from 'engine/pixi';
 
@@ -34,6 +35,8 @@ function LoadingScene() {
 
   storage.set('name', 'Sean');
   console.log(`get data from storage, name = ${storage.get('name')}`);
+
+  console.log(device);
 }
 LoadingScene.prototype = Object.create(Scene.prototype);
 LoadingScene.prototype.constructor = LoadingScene;
