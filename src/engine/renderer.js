add30a30a239252cf79a0a117a1ab9bf764e6f82
Sean Bohan <pixelpicosean@gmail.com>
2015-10-25 09:51:21 +0800

    Create renderer base object, any real renderers should override its methods to work.

---
+++ src/engine/renderer.js
@@ -0,0 +1,8 @@
+let Renderer = {
+  init: function init(width, height, settings) {
+    console.log('[Warning]: No working renderer!');
+  },
+  draw: function draw(scene) {},
+};
+
+export default Renderer;
