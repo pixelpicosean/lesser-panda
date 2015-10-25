51832d2930f190dd8ca52f0bac2ec768e3839b60
Sean Bohan <pixelpicosean@gmail.com>
2015-10-25 17:39:00 +0800

    Add method to add middleware to loader and create ResourceLoader instance when start is called.

--- src/engine/loader.js
+++ src/engine/loader.js
@@ -2,12 +2,18 @@ import EventEmitter from 'engine/eventemitter3';
 import ResourceLoader from 'engine/resource-loader';
 
 // Loader instance
+// TODO: add dymanic loading support
 const loader = new EventEmitter();
 
+let assetsQueue = [];
+
 let loaders = [];
+let middlewares = [];
+
 let loaderIdx = 0;
-let assetsLength = 0;
 let loadedCount = 0;
+
+let resourceLoader = null;
 
 function next() {
   loaderIdx += 1;
@@ -19,9 +25,16 @@ function next() {
   }
 }
 
-// TODO: error and loaded asset info
-function progress() {
+function progress(res, err) {
   loadedCount += 1;
+
+  let assetsLength = 0;
+  for (let l of loaders) {
+    assetsLength += l.getAssetsLength();
+  }
+
+  err && loader.emit('error', err);
+
   loader.emit('progress', loadedCount / assetsLength);
 }
 
@@ -30,44 +43,51 @@ loader.registerLoader = function registerLoader(loader) {
 };
 
 loader.start = function start() {
-  if (loaders.length === 0) {
+  if (assetsQueue.length === 0) {
     loader.emit('complete');
     return;
   }
 
-  for (let l of loaders) {
-    assetsLength += l.getAssetsLength();
+  if (!resourceLoader) {
+    resourceLoader = new loader.ResourceLoader('media');
+    // Load assets
+    resourceLoader.add(assetsQueue);
+
+    // Keep a reference to the resources
+    loader.resources = resourceLoader.resources;
+
+    // Register it as a loader
+    loader.registerLoader({
+      start(onComplete, onProgress) {
+        let mapper = (resLoader, res) => {
+          onProgress(res, res.error);
+        };
+        resourceLoader.on('progress', mapper);
+        resourceLoader.load(() => {
+          setTimeout(onComplete, 0);
+          resourceLoader.off('progress', mapper);
+        });
+      },
+      getAssetsLength() {
+        return Object.keys(resourceLoader.resources).length;
+      },
+    });
   }
 
   loaders[loaderIdx].start(next, progress);
 };
 
-// Internal resource loader
-let resourceLoader = new ResourceLoader();
-loader.registerLoader({
-  start: function(onComplete, onProgress) {
-    resourceLoader.on('load', onProgress);
-    resourceLoader.load(function() {
-      setTimeout(onComplete, 0);
-      resourceLoader.off('load', onProgress);
-    });
-  },
-  getAssetsLength: function() {
-    return Object.keys(resourceLoader.resources).length;
-  },
-});
+loader.ResourceLoader = ResourceLoader;
 
-loader.resourceLoader = resourceLoader;
-
-// TODO: loader middleware support
-
+/**
+ * Add assets to be loaded by ResourceLoader instance
+ */
 loader.addAsset = function addAsset(url, key) {
-  if (key) {
-    resourceLoader.add(key, 'media/' + url);
-  }
-  else {
-    resourceLoader.add('media/' + url);
-  }
+  assetsQueue.push({ url, key })
+};
+
+loader.addMiddleware = function addMiddleware(fn) {
+  middlewares.push(fn);
 };
 
 export default loader;
