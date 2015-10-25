7c84da12baf70ff23faaf3ed299f541aafd83d01
Sean Bohan <pixelpicosean@gmail.com>
2015-10-25 10:45:22 +0800

    Remove draw method of Renderer, inject a "_updateRenderer" method to Scene instead for updating and drawing.

--- src/engine/renderer.js
+++ src/engine/renderer.js
@@ -2,7 +2,6 @@ let Renderer = {
   init: function init(width, height, settings) {
     console.log('[Warning]: No working renderer!');
   },
-  draw: function draw(scene) {},
 };
 
 export default Renderer;
