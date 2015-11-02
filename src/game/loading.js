25658227f336c1ce4df4bb6c651f8b7b9ddc9935
Sean Bohan <pixelpicosean@gmail.com>
2015-11-02 16:44:33 +0800

    Create a loading scene and use it as the one to boot with.

---
+++ src/game/loading.js
@@ -0,0 +1,47 @@
+import engine from 'engine/core';
+import Scene from 'engine/scene';
+import loader from 'engine/loader';
+import PIXI from 'engine/pixi';
+
+const BAR_WIDTH = 200;
+const BAR_HEIGHT = 20;
+
+function Loading() {
+  Scene.call(this);
+
+  this.barBg = new PIXI.Graphics().addTo(this.container);
+  this.barBg.beginFill(0x5f574f);
+  this.barBg.drawRect(0, -BAR_HEIGHT * 0.5, BAR_WIDTH, BAR_HEIGHT);
+  this.barBg.endFill();
+
+  this.bar = new PIXI.Graphics().addTo(this.container);
+  this.bar.beginFill(0xffffff);
+  this.bar.drawRect(0, -BAR_HEIGHT * 0.5, 1, BAR_HEIGHT);
+  this.bar.endFill();
+
+  this.barBg.position = this.bar.position.set(engine.width * 0.5 - BAR_WIDTH * 0.5, engine.height * 0.5);
+}
+Loading.prototype = Object.create(Scene.prototype);
+Object.assign(Loading.prototype, {
+  constructor: Loading,
+  awake() {
+    let redraw = (p) => {
+      this.bar.clear();
+      this.bar.beginFill(0xffffff);
+      this.bar.drawRect(0, -BAR_HEIGHT * 0.5, Math.floor(BAR_WIDTH * p), BAR_HEIGHT);
+      this.bar.endFill();
+    };
+
+    loader.on('progress', redraw);
+    loader.once('complete', function() {
+      loader.off('progress', redraw);
+
+      // Start "Main" scene when assets loaded
+      engine.setScene('Main');
+    }, this);
+
+    loader.start();
+  },
+});
+
+engine.addScene('Loading', Loading);
--- src/game/main.js
+++ src/game/main.js
@@ -1,57 +1,24 @@
 import engine from 'engine/core';
 import Scene from 'engine/scene';
+import PIXI from 'engine/pixi';
 
-import Vector from 'engine/vector';
-import Timeline from 'engine/timeline';
-import physics from 'engine/physics';
+import 'game/loading';
 
-import 'engine/canvasquery';
-
-function Box(x, y, world) {
-  this.body = new physics.Body({
-    mass: 0.4,
-    shape: new physics.Rectangle(20, 20),
-    collisionGroup: 1,
-    collideAgainst: [2],
-  }).addTo(world);
-
-  this.body.position.set(x, y);
-}
-Box.prototype.draw = function draw(gl) {
-  gl.fillStyle('#c04')
-    .fillRect(this.body.position.x - 10, this.body.position.y - 10, 20, 20);
-};
-
-function Wall(x, y, w, h, world) {
-  this.body = new physics.Body({
-    shape: new physics.Rectangle(w, h),
-    collisionGroup: 2,
-  }).addTo(world);
-
-  this.position = this.body.position.set(x, y);
-  this.size = Vector.create(w, h);
-}
-Wall.prototype.draw = function draw(gl) {
-  gl.fillStyle('#0af')
-    .fillRect(this.position.x - this.size.x * 0.5, this.position.y - this.size.y * 0.5, this.size.x, this.size.y);
-};
-
-function LoadingScene() {
+function Main() {
   Scene.call(this);
 
-  this.box = new Box(160, 100, this.world);
-  this.ground = new Wall(160, 180, 320, 20, this.world);
+  this.text = new PIXI.Text('It Works!', {
+    font: '32px Verdana',
+    fill: 'white',
+  }).addTo(this.container);
+  this.text.position.set(engine.width * 0.5 - this.text.width * 0.5, engine.height * 0.5 - this.text.height * 0.5);
+
+  console.log(`engine.size = (${engine.width}, ${engine.height})`);
 }
-LoadingScene.prototype = Object.create(Scene.prototype);
-LoadingScene.prototype.constructor = LoadingScene;
+Main.prototype = Object.create(Scene.prototype);
+Object.assign(Main.prototype, {
+  constructor: Main,
+});
+engine.addScene('Main', Main);
 
-LoadingScene.prototype.draw = function draw(gl) {
-  gl.clear('#203');
-
-  this.box.draw(gl);
-  this.ground.draw(gl);
-};
-
-engine.addScene('LoadingScene', LoadingScene);
-
-engine.startWithScene('LoadingScene');
+engine.startWithScene('Loading');
