0c0cb43568a5ce00698cce983b1f68bc190b1e19
Sean Bohan <pixelpicosean@gmail.com>
2015-11-16 20:31:41 +0800

    Use class instead of function to define the Main scene.

--- src/game/main.js
+++ src/game/main.js
@@ -4,21 +4,20 @@ import PIXI from 'engine/pixi';
 
 import 'game/loading';
 
-function Main() {
-  Scene.call(this);
+class Main extends Scene {
+  constructor() {
+    super();
 
-  this.text = new PIXI.Text('It Works!', {
-    font: '32px Verdana',
-    fill: 'white',
-  }).addTo(this.container);
-  this.text.position.set(engine.width * 0.5 - this.text.width * 0.5, engine.height * 0.5 - this.text.height * 0.5);
+    console.log(`engine.size = (${engine.width}, ${engine.height})`);
+    console.log(`engine.viewSize = (${engine.viewWidth}, ${engine.viewHeight})`);
 
-  console.log(`engine.size = (${engine.width}, ${engine.height})`);
-}
-Main.prototype = Object.create(Scene.prototype);
-Object.assign(Main.prototype, {
-  constructor: Main,
-});
+    let text = new PIXI.Text('It Works!', {
+      font: '24px Verdana',
+      fill: 'white',
+    }, window.devicePixelRatio).addTo(this.container);
+    text.position.set(engine.width * 0.5 - text.width * 0.5, engine.height * 0.5 - text.height * 0.5);
+  }
+};
 engine.addScene('Main', Main);
 
 engine.startWithScene('Loading');
