385e036b1175e9fd8f22d0007cfbb0e66c8de8d0
Sean Bohan <pixelpicosean@gmail.com>
2015-12-20 09:13:05 +0800

    Use loader and bitmap font for template game.

--- src/game/main.js
+++ src/game/main.js
@@ -4,15 +4,18 @@ import PIXI from 'engine/pixi';
 
 import 'game/loading';
 
+import loader from 'engine/loader';
+
+loader.addAsset('KenPixel.fnt');
+
 class Main extends Scene {
   constructor() {
     super();
 
     // Use the better Text instead
-    let text = new PIXI.extras.Text('It Works!', {
-      font: '24px Verdana',
-      fill: 'white',
-    }, window.devicePixelRatio).addTo(this.stage);
+    let text = new PIXI.extras.BitmapText('It Works!', {
+      font: '24px KenPixel',
+    }).addTo(this.stage);
     text.position.set(engine.width * 0.5 - text.width * 0.5, engine.height * 0.5 - text.height * 0.5);
   }
 };
