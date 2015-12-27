ec0520eaa7333ec8dca38647ff5c06bd382ecd18
Sean Bohan <pixelpicosean@gmail.com>
2015-12-27 18:07:55 +0800

    Add animation example to demo game.

--- src/game/main.js
+++ src/game/main.js
@@ -8,7 +8,14 @@ import 'engine/timeline';
 
 import 'game/loading';
 
+// Load textures
+loader.addAsset('gold_1.png', 'gold_1');
+loader.addAsset('gold_2.png', 'gold_2');
+loader.addAsset('gold_3.png', 'gold_3');
+loader.addAsset('gold_4.png', 'gold_4');
+// Load bitmap fonts
 loader.addAsset('KenPixel.fnt');
+// Load audio files
 loader.addSound(['boot.m4a', 'boot.ogg'], 'bgm');
 
 PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
@@ -28,6 +35,17 @@ class Main extends Scene {
       .repeat(3);
 
     Timer.later(200, () => audio.sounds['bgm'].play());
+
+    const anim = new PIXI.extras.Animation([
+      PIXI.Texture.fromAsset('gold_1'),
+      PIXI.Texture.fromAsset('gold_2'),
+      PIXI.Texture.fromAsset('gold_3'),
+      PIXI.Texture.fromAsset('gold_4'),
+    ]).addTo(this.stage);
+    anim.position.set(engine.width * 0.5, 50);
+    anim.anchor.set(0.5);
+    anim.addAnim('rotate', [0, 1, 2, 3, 2, 1], { speed: 12 });
+    anim.play('rotate');
   }
 };
 engine.addScene('Main', Main);
