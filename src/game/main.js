3096b84540653bc4a3666bfa1cd5520b7d96c47f
Sean Bohan <pixelpicosean@gmail.com>
2016-03-10 21:24:38 +0800

    Update template game.

--- src/game/main.js
+++ src/game/main.js
@@ -4,7 +4,7 @@ import PIXI from 'engine/pixi';
 import Timer from 'engine/timer';
 import audio from 'engine/audio';
 import loader from 'engine/loader';
-import 'engine/timeline';
+import { Action } from 'engine/animation';
 
 import 'game/loading';
 
@@ -24,23 +24,25 @@ class Main extends Scene {
   constructor() {
     super();
 
+    // PIXI instance
     let text = new PIXI.extras.BitmapText('It Works!', {
       font: '32px KenPixel',
     }).addTo(this.stage);
     text.y = engine.height * 0.5;
     this.info = text;
 
-    this.addTimeline(text)
-      .wait(2000)
+    // Both number and string text can be tweened
+    this.tween(text)
+      .to({ 'scale.x': 1.2, 'scale.y': 1.2 }, 200)
+      .to({ 'scale.x': 1.0, 'scale.y': 1.0 }, 200)
+      .repeat(3)
+      .wait(1000)
       .to({ text: 'A Sean Production!' }, 2000);
-
-    this.addTimeline(text.scale)
-      .to({ x: [1.2, 1.0], y: [1.2, 1.0] }, 200)
-      .repeat(3);
 
     Timer.later(200, () => audio.sounds['bgm'].play());
 
-    const anim = new PIXI.extras.Animation([
+    // AnimatedSprite with frames
+    const anim = new PIXI.extras.AnimatedSprite([
       PIXI.Texture.fromAsset('gold_1'),
       PIXI.Texture.fromAsset('gold_2'),
       PIXI.Texture.fromAsset('gold_3'),
@@ -50,6 +52,15 @@ class Main extends Scene {
     anim.anchor.set(0.5);
     anim.addAnim('rotate', [0, 1, 2, 3, 2, 1], { speed: 12 });
     anim.play('rotate');
+
+    // Blender/Flash like keyframe based animation
+    const moveAct = Action.create()
+      .channel('position.x')
+        .key(0, engine.width * 0.5)
+        .key(1000, engine.width * 0.8)
+        .key(3000, engine.width * 0.2)
+        .key(4000, engine.width * 0.5);
+    this.runAction(moveAct, anim);
   }
   update() {
     this.info.x = engine.width * 0.5 - this.info.width * 0.5;
