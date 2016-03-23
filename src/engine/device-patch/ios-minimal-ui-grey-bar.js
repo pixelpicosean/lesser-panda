3168e3e786ed8493523e0213d997be57f90ccbd4
Sean Bohan <pixelpicosean@gmail.com>
2016-03-23 16:38:10 +0800

    Fix iOS9 patch.

--- src/engine/device-patch/ios-minimal-ui-grey-bar.js
+++ src/engine/device-patch/ios-minimal-ui-grey-bar.js
@@ -1,10 +1,10 @@
 var device = require('engine/device');
 
-if (device.ios) {
+if (device.iOS) {
   window.addEventListener('scroll', function () {
     // Do not scroll when keyboard is visible
     if (document.activeElement === document.body && window.scrollY > 0) {
-      document.body.scrollTop = 0;
+      document.body.scrollTop = (device.iOS9 && device.iPhone) ? 44 : 0;
     }
   }, true);
 }
