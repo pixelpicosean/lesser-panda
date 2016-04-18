d323a516b2341bebfe6b2a5b00565c8b90e4a3f7
Sean Bohan <pixelpicosean@gmail.com>
2016-04-18 09:50:44 +0800

    Create a box for collision test.
    Not work yet.

--- src/game/main.js
+++ src/game/main.js
@@ -10,14 +10,16 @@ import Tilemap from 'engine/tilemap';
 import config from 'game/config';
 import 'game/loading';
 
+import PrimitiveActor from 'engine/actors/primitive-actor';
+
 loader.addAsset('tileset.png');
 loader.addAsset('room.json');
 
 // Constants
 const GROUPS = {
-  SOLID:  0,
-  BOX:    1,
-  CIRCLE: 2,
+  SOLID:  physics.getGroupMask(0),
+  BOX:    physics.getGroupMask(1),
+  CIRCLE: physics.getGroupMask(2),
 };
 
 PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
@@ -38,6 +40,18 @@ class Main extends Scene {
     const map = loader.resources['room.json'].data;
     const tilemap2 = Tilemap.fromTiledJson(map, tilesets, GROUPS.SOLID)
       .addTo(this, this.bottomLayer);
+
+    const box = new PrimitiveActor('Box', 0xfff4ed, 8).addTo(this, this.bottomLayer);
+    box.mass = 0.01;
+    box.collisionGroup = GROUPS.BOX;
+    box.collideAgainst = [GROUPS.SOLID];
+    box.body.collide = (other) => {
+      if (other.collisionGroup === GROUPS.SOLID) {
+        console.log('collide');
+        return true;
+      }
+    };
+    box.position.set(16, 144);
   }
 };
 engine.addScene('Main', Main);
