348aec73fc02c4540c6b2f52071f7fef5dceb782
Sean Bohan <pixelpicosean@gmail.com>
2016-04-19 11:42:52 +0800

    Fix ES6 keyword issue.

--- src/engine/tilemap/filmstrip.js
+++ src/engine/tilemap/filmstrip.js
@@ -1,18 +1,18 @@
 var PIXI = require('engine/pixi');
 
 module.exports = exports = function filmstrip(tileset, tileWidth, tileHeight) {
-  let strip = [];
+  var strip = [];
 
-  let w = tileset.width;
-  let h = tileset.height;
-  let crop = tileset.crop;
+  var w = tileset.width;
+  var h = tileset.height;
+  var crop = tileset.crop;
 
-  let sheet = tileset.baseTexture;
+  var sheet = tileset.baseTexture;
 
-  let cols = Math.floor(w / tileWidth);
-  let rows = Math.floor(h / tileHeight);
+  var cols = Math.floor(w / tileWidth);
+  var rows = Math.floor(h / tileHeight);
 
-  let q = 0, r = 0;
+  var q = 0, r = 0;
   for (r = 0; r < rows; r++) {
     for (q = 0; q < cols; q++) {
       strip.push(new PIXI.Texture(sheet, new PIXI.Rectangle(q * tileWidth + crop.x, r * tileHeight + crop.y, tileWidth, tileHeight)));
