c12e7d1ea82b89116910d2ff38864e3cc4e852db
Sean Bohan <pixelpicosean@gmail.com>
2016-04-30 13:54:05 +0800

    Change anchor-to-screen behavior to new design.

--- src/behaviors/anchor-to-screen.js
+++ src/behaviors/anchor-to-screen.js
@@ -5,25 +5,10 @@
  * Note: `right` will override `left`, and
  * `bottom` will over `top`. So only zero or one property
  * should be set on each axis.
- *
- * @protocol {
- *   position: Vector
- * }
  */
 
 import engine from 'engine/core';
 import Behavior from 'engine/behavior';
-
-const settings = {
-  left: undefined,
-  right: undefined,
-  top: undefined,
-  bottom: undefined,
-
-  // TODO: take bounds into account
-};
-
-const setupTarget = function() {};
 
 function pct2Num(pctStr) {
   if (pctStr.length === 0) return 0;
@@ -33,46 +18,51 @@ function pct2Num(pctStr) {
 }
 
 export default class AnchorToScreen extends Behavior {
-  constructor(s) {
-    super('AnchorToScreen', setupTarget, Object.assign({}, settings, s), true);
+  defaultSettings = {
+    left: undefined,
+    right: undefined,
+    top: undefined,
+    bottom: undefined,
+  }
+
+  setup(settings) {
+    super.setup(settings);
 
     this.calc('left');
     this.calc('right');
     this.calc('top');
     this.calc('bottom');
   }
-  activate() {
-    this.applyAnchor();
-    return super.activate();
-  }
   update() {
     this.applyAnchor();
   }
   applyAnchor() {
+    const bounds = this.target.sprite.getLocalBounds();
+
     if (this.left !== undefined) {
-      this.target.position.x = this.left;
+      this.target.position.x = this.left + bounds.x;
     }
     else if (this.leftPct !== undefined) {
-      this.target.position.x = engine.width * this.leftPct;
+      this.target.position.x = engine.width * this.leftPct - bounds.x;
     }
     if (this.right !== undefined) {
-      this.target.position.x = engine.width - this.right;
+      this.target.position.x = engine.width - this.right - (bounds.x + bounds.width);
     }
     else if (this.rightPct !== undefined) {
-      this.target.position.x = engine.width * (1 - this.rightPct);
+      this.target.position.x = engine.width * (1 - this.rightPct) - (bounds.x + bounds.width);
     }
 
     if (this.top !== undefined) {
-      this.target.position.y = this.top;
+      this.target.position.y = this.top + bounds.y;
     }
     else if (this.topPct !== undefined) {
-      this.target.position.y = engine.height * this.topPct;
+      this.target.position.y = engine.height * this.topPct - bounds.y;
     }
     if (this.bottom !== undefined) {
-      this.target.position.y = engine.height - this.bottom;
+      this.target.position.y = engine.height - this.bottom - (bounds.y + bounds.height);
     }
     else if (this.bottomPct !== undefined) {
-      this.target.position.y = engine.height * (1 - this.bottomPct);
+      this.target.position.y = engine.height * (1 - this.bottomPct) - (bounds.y + bounds.height);
     }
   }
 
@@ -83,3 +73,5 @@ export default class AnchorToScreen extends Behavior {
     }
   }
 }
+
+Behavior.register('AnchorToScreen', AnchorToScreen);
