afc1b4a64ce362050835dcf4f785c532cea7200f
Sean Bohan <pixelpicosean@gmail.com>
2016-04-30 14:27:04 +0800

    Change four-way-move behavior to new design.

--- src/behaviors/four-way-move.js
+++ src/behaviors/four-way-move.js
@@ -1,73 +1,32 @@
 /**
- * Make the target object able to move horizontally or vertically
- *
- * @protocol {
- *   position: Vector
- * }
+ * Make the Actor able to move horizontally or vertically.
  */
 
 import keyboard from 'engine/keyboard';
 import Behavior from 'engine/behavior';
 import Vector from 'engine/vector';
 
-const settings = {
-  /* Move speed */
-  speed: 200,
+export default class FourWayMove extends Behavior {
+  type = 'FourWayMove'
 
-  /* Whether use keyboard to control */
-  useKeyboard: true,
-  /* Hold to move left, when `useKeyboard` is true */
-  leftKey: 'LEFT',
-  /* Hold to move right, when `useKeyboard` is true */
-  rightKey: 'RIGHT',
-  /* Hold to move up, when `useKeyboard` is true */
-  upKey: 'UP',
-  /* Hold to move down, when `useKeyboard` is true */
-  downKey: 'DOWN',
-};
+  defaultSettings = {
+    /* Move speed */
+    speed: 200,
 
-// Move left
-function moveLeft() {
-  this.FourWayMove.dir.x = -1;
-}
-// Move right
-function moveRight() {
-  this.FourWayMove.dir.x = 1;
-}
-// Move up
-function moveUp() {
-  this.FourWayMove.dir.y = -1;
-}
-// Move down
-function moveDown() {
-  this.FourWayMove.dir.y = 1;
-}
-// Stop horizontal movement
-function stopX() {
-  this.FourWayMove.dir.x = 0;
-}
-// Stop vertical movement
-function stopY() {
-  this.FourWayMove.dir.y = 0;
-}
-// Stop
-function stop() {
-  this.FourWayMove.dir.set(0);
-}
+    /* Whether use keyboard to control */
+    useKeyboard: true,
+    /* Hold to move left, when `useKeyboard` is true */
+    leftKey: 'LEFT',
+    /* Hold to move right, when `useKeyboard` is true */
+    rightKey: 'RIGHT',
+    /* Hold to move up, when `useKeyboard` is true */
+    upKey: 'UP',
+    /* Hold to move down, when `useKeyboard` is true */
+    downKey: 'DOWN',
+  }
 
-const setupTarget = function() {
-  this.moveLeft = moveLeft;
-  this.moveRight = moveRight;
-  this.moveUp = moveUp;
-  this.moveDown = moveDown;
-  this.stopX = stopX;
-  this.stopY = stopY;
-  this.stop = stop;
-};
-
-export default class VerticalMove extends Behavior {
-  constructor(s) {
-    super('VerticalMove', setupTarget, Object.assign({}, settings, s), true);
+  constructor() {
+    super();
 
     this.dir = Vector.create();
     this.left = false;
@@ -89,4 +48,36 @@ export default class VerticalMove extends Behavior {
     this.target.position.x += this.dir.x * this.speed * dt;
     this.target.position.y += this.dir.y * this.speed * dt;
   }
+
+  // Actors
+  // Move left
+  moveLeft() {
+    this.dir.x = -1;
+  }
+  // Move right
+  moveRight() {
+    this.dir.x = 1;
+  }
+  // Move up
+  moveUp() {
+    this.dir.y = -1;
+  }
+  // Move down
+  moveDown() {
+    this.dir.y = 1;
+  }
+  // Stop horizontal movement
+  stopX() {
+    this.dir.x = 0;
+  }
+  // Stop vertical movement
+  stopY() {
+    this.dir.y = 0;
+  }
+  // Stop
+  stop() {
+    this.dir.set(0);
+  }
 }
+
+Behavior.register('FourWayMove', FourWayMove);
