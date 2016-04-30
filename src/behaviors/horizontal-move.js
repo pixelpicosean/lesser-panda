7fd5e726f91264c62c5b8b3b4858aedc8f389ce9
Sean Bohan <pixelpicosean@gmail.com>
2016-04-30 14:25:18 +0800

    Change two way move behaviors to new design.

--- src/behaviors/horizontal-move.js
+++ src/behaviors/horizontal-move.js
@@ -9,61 +9,45 @@
 import keyboard from 'engine/keyboard';
 import Behavior from 'engine/behavior';
 
-const settings = {
-  /* Move speed */
-  speed: 200,
-
-  /* Range of the movement, limit to a range or keep it undefined to avoid */
-  range: undefined,
-  /**
-   * Percentage of start x location in the range if exist
-   * when range is defined
-   */
-  startPct: 0,
-
-  /* Whether use keyboard to control */
-  useKeyboard: true,
-  /* Hold to move left, when `useKeyboard` is true */
-  leftKey: 'LEFT',
-  /* Hold to move right, when `useKeyboard` is true */
-  rightKey: 'RIGHT',
-};
-
-// Start to move left
-function moveLeft() {
-  this.HorizontalMove.dir = -1;
-}
-// Start to move right
-function moveRight() {
-  this.HorizontalMove.dir = 1;
-}
-// Stop
-function stop() {
-  this.HorizontalMove.dir = 0;
-}
-
-const setupTarget = function() {
-  this.moveLeft = moveLeft;
-  this.moveRight = moveRight;
-  this.stop = stop;
-};
-
 export default class HorizontalMove extends Behavior {
-  constructor(s) {
-    super('HorizontalMove', setupTarget, Object.assign({}, settings, s), true);
+  type = 'HorizontalMove'
+
+  defaultSettings = {
+    /* Move speed */
+    speed: 200,
+
+    /* Range of the movement, limit to a range or keep it undefined to avoid */
+    range: undefined,
+    /**
+     * Percentage of start x location in the range if exist
+     * when range is defined
+     */
+    startPct: 0,
+
+    /* Whether use keyboard to control */
+    useKeyboard: true,
+    /* Hold to move left, when `useKeyboard` is true */
+    leftKey: 'LEFT',
+    /* Hold to move right, when `useKeyboard` is true */
+    rightKey: 'RIGHT',
+  }
+
+  constructor() {
+    super();
 
     this.dir = 0;
     this.left = 0;
     this.right = 0;
-    this.hasRange = Number.isFinite(this.range);
+    this.hasRange = false;
   }
-  activate() {
+  setup(settings) {
+    super.setup(settings);
+
+    this.hasRange = Number.isFinite(this.range);
     if (this.hasRange) {
       this.left = this.target.position.x - this.range * this.startPct;
       this.right = this.left + this.range;
     }
-
-    return super.activate();
   }
   update(_, dt) {
     if (this.useKeyboard) {
@@ -78,13 +62,29 @@ export default class HorizontalMove extends Behavior {
       if (this.target.position.x > this.right) {
         this.target.position.x = this.right;
         this.dir = 0;
-        this.emit('reachEnd');
+        this.target.emit('reachEnd');
       }
       else if (this.target.position.x < this.left) {
         this.target.position.x = this.left;
         this.dir = 0;
-        this.emit('reachStart');
+        this.target.emit('reachStart');
       }
     }
   }
+
+  // Actions
+  // Start to move left
+  moveLeft() {
+    this.dir = -1;
+  }
+  // Start to move right
+  moveRight() {
+    this.dir = 1;
+  }
+  // Stop
+  stop() {
+    this.dir = 0;
+  }
 }
+
+Behavior.register('HorizontalMove', HorizontalMove);
--- src/behaviors/vertical-move.js
+++ src/behaviors/vertical-move.js
@@ -9,61 +9,45 @@
 import keyboard from 'engine/keyboard';
 import Behavior from 'engine/behavior';
 
-const settings = {
-  /* Move speed */
-  speed: 200,
-
-  /* Range of the movement, limit to a range or keep it undefined to avoid */
-  range: undefined,
-  /**
-   * Percentage of start y location in the range if exist
-   * when range is defined
-   */
-  startPct: 0,
-
-  /* Whether use keyboard to control */
-  useKeyboard: true,
-  /* Hold to move up, when `useKeyboard` is true */
-  upKey: 'UP',
-  /* Hold to move down, when `useKeyboard` is true */
-  downKey: 'DOWN',
-};
-
-// Start to move up
-function moveUp() {
-  this.VerticalMove.dir = -1;
-}
-// Start to move down
-function moveDown() {
-  this.VerticalMove.dir = 1;
-}
-// Stop
-function stop() {
-  this.VerticalMove.dir = 0;
-}
-
-const setupTarget = function() {
-  this.moveUp = moveUp;
-  this.moveDown = moveDown;
-  this.stop = stop;
-};
-
 export default class VerticalMove extends Behavior {
-  constructor(s) {
-    super('VerticalMove', setupTarget, Object.assign({}, settings, s), true);
+  type = 'VerticalMove'
+
+  defaultSettings = {
+    /* Move speed */
+    speed: 200,
+
+    /* Range of the movement, limit to a range or keep it undefined to avoid */
+    range: undefined,
+    /**
+     * Percentage of start y location in the range if exist
+     * when range is defined
+     */
+    startPct: 0,
+
+    /* Whether use keyboard to control */
+    useKeyboard: true,
+    /* Hold to move up, when `useKeyboard` is true */
+    upKey: 'UP',
+    /* Hold to move down, when `useKeyboard` is true */
+    downKey: 'DOWN',
+  }
+
+  constructor() {
+    super();
 
     this.dir = 0;
     this.top = 0;
     this.bottom = 0;
-    this.hasRange = Number.isFinite(this.range);
+    this.hasRange = false;
   }
-  activate() {
+  setup(settings) {
+    super.setup(settings);
+
+    this.hasRange = Number.isFinite(this.range);
     if (this.hasRange) {
       this.top = this.target.position.y - this.range * this.startPct;
       this.bottom = this.top + this.range;
     }
-
-    return super.activate();
   }
   update(_, dt) {
     if (this.useKeyboard) {
@@ -78,13 +62,29 @@ export default class VerticalMove extends Behavior {
       if (this.target.position.y > this.bottom) {
         this.target.position.y = this.bottom;
         this.dir = 0;
-        this.emit('reachEnd');
+        this.target.emit('reachEnd');
       }
       else if (this.target.position.y < this.top) {
         this.target.position.y = this.top;
         this.dir = 0;
-        this.emit('reachStart');
+        this.target.emit('reachStart');
       }
     }
   }
+
+  // Actions
+  // Start to move up
+  moveUp() {
+    this.dir = -1;
+  }
+  // Start to move down
+  moveDown() {
+    this.dir = 1;
+  }
+  // Stop
+  stop() {
+    this.dir = 0;
+  }
 }
+
+Behavior.register('VerticalMove', VerticalMove);
