6f2cfc63e6845d00045276bd502b0ec17b1d1e91
Sean Bohan <pixelpicosean@gmail.com>
2016-06-05 08:27:04 +0800

    Use new `createLayer` in game template and update changelog.

--- src/game/main.js
+++ src/game/main.js
@@ -11,9 +11,15 @@ PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
 
 class Main extends Scene {
   awake() {
+    this
+      .createLayer('worldLayer')
+        .createLayer('actLayer', 'worldLayer')
+        .createLayer('fxLayer', 'worldLayer')
+      .createLayer('uiLayer');
+
     const text = new PIXI.extras.BitmapText('It Works!', {
       font: '16px KenPixel',
-    }).addTo(this.stage);
+    }).addTo(this.uiLayer);
     text.position
       .set(core.width * 0.5, core.height * 0.5)
       .subtract(text.width * 0.5, text.height * 0.5);
