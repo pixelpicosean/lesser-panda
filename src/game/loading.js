4fc4585c042fe090c569cd0ff1bbaaa28db011dc
Sean Bohan <pixelpicosean@gmail.com>
2016-10-31 14:34:54 +0800

    Create a basic loading screen.

--- src/game/loading.js
+++ src/game/loading.js
@@ -1,52 +1,60 @@
-import engine from 'engine/core';
-import Scene from 'engine/scene';
-import loader from 'engine/loader';
-import PIXI from 'engine/pixi';
+const core = require('engine/core');
+const Game = require('engine/game');
+const loader = require('engine/loader');
+const SystemGfx = require('engine/gfx');
+const graphics = require('engine/gfx/graphics');
+const text = require('engine/gfx/text');
 
-import config from 'game/config';
+const BAR_WIDTH = Math.floor(core.width * 0.75);
+const BAR_HEIGHT = Math.floor(BAR_WIDTH * 0.075);
 
-const BAR_WIDTH = 200;
-const BAR_HEIGHT = 20;
-
-class Loading extends Scene {
+class Loading extends Game {
   constructor() {
     super();
 
-    this.barBg = new PIXI.Graphics().addTo(this.stage);
+    this.addSystem(new SystemGfx());
+
+    this.barBg = graphics({}).addTo(this.sGfx.root);
+    this.barBg.clear();
     this.barBg.beginFill(0x5f574f);
     this.barBg.drawRect(0, -BAR_HEIGHT * 0.5, BAR_WIDTH, BAR_HEIGHT);
     this.barBg.endFill();
 
-    this.bar = new PIXI.Graphics().addTo(this.stage);
+    this.bar = graphics({}).addTo(this.sGfx.root);
+    this.bar.clear();
     this.bar.beginFill(0xffffff);
     this.bar.drawRect(0, -BAR_HEIGHT * 0.5, 1, BAR_HEIGHT);
     this.bar.endFill();
 
-    this.barBg.position = this.bar.position.set(engine.width * 0.5 - BAR_WIDTH * 0.5, engine.height * 0.5);
+    this.pct = text({
+      text: '100%',
+      font: `${BAR_HEIGHT - 2}px Verdana`,
+      fill: '#ffffff',
+      anchor: { x: 0.5, y: 0.5 },
+      position: { x: core.width / 2, y: core.height / 2 - BAR_HEIGHT * 1.5 },
+    }).addTo(this.sGfx.root);
+
+    this.barBg.position = this.bar.position.set(core.width / 2 - BAR_WIDTH / 2, core.height / 2);
   }
 
-  awake() {
-    let redraw = (p) => {
+  awake({ gameClass }) {
+    let redraw = () => {
+      this.pct.text = `${loader.progress}%`;
+
       this.bar.clear();
       this.bar.beginFill(0xffffff);
-      this.bar.drawRect(0, -BAR_HEIGHT * 0.5, Math.floor(BAR_WIDTH * p), BAR_HEIGHT);
+      this.bar.drawRect(0, -BAR_HEIGHT * 0.5, Math.floor(BAR_WIDTH * loader.progress * 0.01), BAR_HEIGHT);
       this.bar.endFill();
     };
 
-    loader.on('progress', redraw);
-    loader.once('complete', function() {
-      loader.off('progress', redraw);
-
-      // Do any assets post process
-      // (such as saving textures, creating spritesheet textures...)
-      // before switch to the "firstScene"
-
-      // Start "Main" scene when assets loaded
-      engine.setScene(config.firstScene || 'Main');
+    let h = loader.onProgress.add(redraw);
+    loader.onComplete.once(function() {
+      h.detach();
+      core.setGame(gameClass, true);
     });
 
-    loader.start();
+    loader.load();
   }
 };
 
-engine.addScene('Loading', Loading);
+module.exports = Loading;
