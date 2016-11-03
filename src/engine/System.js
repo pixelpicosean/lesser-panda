cd3433c07f79168fec57858de3e78eb556cbdc7e
Sean Bohan <pixelpicosean@gmail.com>
2016-11-03 22:07:44 +0800

    Add some JSDoc comments to abstract system interface.

--- src/engine/System.js
+++ src/engine/System.js
@@ -1,17 +1,28 @@
 class System {
   constructor() {
+    /**
+     * Name of this system
+     * @type {String}
+     * @readonly
+     */
     this.name = '';
+
+    /**
+     * Reference to the owner game instance
+     * @type {Game}
+     * @readonly
+     */
     this.game = null;
   }
 
-  awake() {}
-  update() {}
-  fixedUpdate() {}
+  awake(settings) {} /* eslint no-unused-vars:0 */
+  update(dt, dtSec) {} /* eslint no-unused-vars:0 */
+  fixedUpdate(dt, dtSec) {} /* eslint no-unused-vars:0 */
   freeze() {}
 
-  onEntitySpawn() {}
-  onEntityRemove() {}
-  onEntityTagChange() {}
+  onEntitySpawn(ent) {} /* eslint no-unused-vars:0 */
+  onEntityRemove(ent) {} /* eslint no-unused-vars:0 */
+  onEntityTagChange(ent, tag) {} /* eslint no-unused-vars:0 */
 
   onPause() {}
   onResume() {}
