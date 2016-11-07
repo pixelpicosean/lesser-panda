343a8f66eef1af12022a637914e0ea0ee84a31cb
Sean Bohan <pixelpicosean@gmail.com>
2016-11-07 20:17:07 +0800

    Code cleanup.

--- src/game/main.js
+++ src/game/main.js
@@ -1,17 +1,13 @@
 const core = require('engine/core');
 const loader = require('engine/loader');
 const Game = require('engine/Game');
+
+// Systems
 const SystemGfx = require('engine/gfx');
 const BitmapText = require('engine/gfx/BitmapText');
 
+// Loading screen
 const Loading = require('game/Loading');
-
-const SystemPhysics = require('engine/physics');
-const AABBSolver = require('engine/physics/AABBSolver');
-const Collider = require('engine/physics/Collider');
-const CollisionMap = require('engine/physics/CollisionMap');
-
-const keyboard = require('engine/keyboard');
 
 // Resource loading
 loader.add('KenPixel.fnt');
@@ -20,49 +16,15 @@ class MyGame extends Game {
   constructor() {
     super();
 
-    this
-      .addSystem(new SystemGfx())
-      .addSystem(new SystemPhysics({
-        solver: new AABBSolver(),
-      }));
+    this.addSystem(new SystemGfx());
 
     this.sysGfx.createLayer('background');
 
-    this.info = BitmapText({
-      text: '()',
+    let t = BitmapText({
+      text: 'It Works!',
       font: '16px KenPixel',
     }).addTo(this.sysGfx.layers['background']);
-    this.info.position.set(20, 20);
-
-    let map = CollisionMap(16, [
-      [1,1,1,1],
-      [1,0,0,1],
-      [1,0,0,1],
-      [1,1,1,1],
-    ]);
-    this.sysPhysics.collisionMap = map;
-    this.c = Collider({
-      shape: 'Box',
-      width: 18, height: 18,
-      position: { x: 32, y: 32 },
-    });
-    this.sysPhysics.addCollider(this.c);
-  }
-  fixedUpdate(_, dt) {
-    super.fixedUpdate(_, dt);
-
-    if (keyboard.down('LEFT')) {
-      this.c.velocity.x = -5;
-    }
-    else if (keyboard.down('RIGHT')) {
-      this.c.velocity.x = +5;
-    }
-    else {
-      this.c.velocity.x = 0;
-    }
-
-    this.info.text = `pos(${this.c.position.x}, ${this.c.position.y})
-vel(${this.c.velocity.x}, ${this.c.velocity.y})`;
+    t.position.set(core.width / 2 - t.width / 2, core.height / 2 - t.height / 2);
   }
 }
 
