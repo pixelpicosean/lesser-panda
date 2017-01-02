8ea6722201b900e0eecf906945bfcb89ee2125c7
Sean Bohan <pixelpicosean@gmail.com>
2017-01-02 18:05:24 +0800

    Bump version.

--- src/engine/core.js
+++ src/engine/core.js
@@ -20,7 +20,7 @@ Object.assign(core, {
    * @memberof module:engine/core
    * @type {string}
    */
-  version: 'v1.2.1',
+  version: 'v1.2.2-dev',
 
   /**
    * Set to `false` to disable version info console output.
